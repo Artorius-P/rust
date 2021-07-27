c2d27bcc0d0b642188d5d0f792786c4ed0a1052b
Artorius <artorius-p@outlook.com>
2021-07-27 22:26:31 +0800

    做出来了。

--- src/first_missing_positive.rs
+++ src/first_missing_positive.rs
@@ -1,27 +1,33 @@
 fn main() {
-    let nums = vec![-1, 4, 2, 1, 9, 10];
+    let nums = vec![1];
     println!("{}", first_missing_positive(nums));
 
 }
 
 fn first_missing_positive(mut nums: Vec<i32>) -> i32 {
+
+    let times_flag = 0;
     for i in 0..nums.len() {
         let element = nums[i].clone();
-        swap(&mut nums, i as i32, element);
+        swap(&mut nums, i as i32, element, times_flag);
     }
-    for i in 0..nums.len() {
-        print!("{} ", nums[i]);
-    }
+
     for i in 0..nums.len() {
         if nums[i] - 1 != i as i32 {
             return (i + 1)as i32;
         }
     }
-    return nums.len()as i32;
+    return (nums.len()+1) as i32;
 }
 
-fn swap(nums: &mut Vec<i32>, subscript: i32, element: i32) {
 
+fn swap(nums: &mut Vec<i32>, subscript: i32, element: i32, mut times_flag: i32) {
+
+    times_flag += 1;
+
+    if times_flag > nums.len() as i32 {
+        return;
+    }
     //元素不在范围内
     if element <= 0 {
         return;
@@ -37,9 +43,9 @@ fn swap(nums: &mut Vec<i32>, subscript: i32, element: i32) {
     }
 
     //元素-1与下标不等，
-    let tmp = nums[element as usize];
-    nums[element as usize] = element;
+    let tmp = nums[(element-1) as usize];
+    nums[(element-1) as usize] = element;
     nums[subscript as usize] = tmp;
     
-    swap(nums, subscript, nums[subscript as usize]);
+    swap(nums, subscript, nums[subscript as usize], times_flag);
 }
